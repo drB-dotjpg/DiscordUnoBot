88a4700f09161ca09e5770695bbe3091ef1e6e6f
jpg <derek.bond24@gmail.com>
2019-11-20 16:00:19 -0600

    Add project files.

---
+++ DiscordUnoBot/Program.cs
@@ -0,0 +1,40 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+
+namespace DiscordUnoBot
+{
+    class Program
+    {
+        enum CardColor { Red, Blue, Yellow, Green, All }
+        enum CardType { Number, Skip, DrawTwo, Reverse, Wild, WildDrawFour }
+
+        class Player
+        {
+            public SocketUser thisUser { get; }
+            public string name { get; }
+            public List<Card> cards { get; set; } = new List<Card>();
+
+            public Player(SocketUser user)
+            {
+                thisUser = user;
+                name = user.Username;
+            }
+        }
+        class Card
+        {
+            public CardColor color { get; }
+            public CardType type { get; }
+
+            public Card(CardColor color, CardType type)
+            {
+                this.color = color;
+                this.type = type;
+            }
+        }
+        class Game
+        {
+
+        }
+    }
+}
